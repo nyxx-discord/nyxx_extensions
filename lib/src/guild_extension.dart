a7fad57e588be9c6ef80bffbed542e0b052c7bdb
Rapougnac <74512338+Rapougnac@users.noreply.github.com>
2022-11-14 23:20:59 +0100

    fix: pattern matching

--- lib/src/guild_extension.dart
+++ lib/src/guild_extension.dart
@@ -3,6 +3,6 @@ import 'package:nyxx/nyxx.dart';
 extension GuildExtension on IGuild {
   /// The acronym of the guild if no icon is chosen.
   String get acronym {
-    return name.replaceAll(r"'s ", '').replaceAllMapped(r'\w+', (match) => match.group(0)!).replaceAll(r'\s', '');
+    return name.replaceAll(r"'s ", ' ').replaceAllMapped(RegExp(r'\w+'), (match) => match[0]![0]).replaceAll(RegExp(r'\s'), '');
   }
 }
