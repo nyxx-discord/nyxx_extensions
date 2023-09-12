4c385c67ad3b494608bfa25cb6266f809cb4e0b2
Rapougnac <me@rapougnac.moe>
2023-09-12 13:19:36 +0200

    fix internal nullable extensions

--- lib/src/embed_builder.dart
+++ lib/src/embed_builder.dart
@@ -3,13 +3,13 @@ import 'package:nyxx/nyxx.dart';
 extension EmbedExtension on Embed {
   EmbedBuilder toEmbedBuilder() {
     return EmbedBuilder(
-      author: author._toEmbedAuthorBuilder(),
+      author: author?._toEmbedAuthorBuilder(),
       color: color,
       description: description,
       fields: fields?.map((field) => field._toEmbedFieldBuilder()).toList(),
-      footer: footer._toEmbedFooterBuilder(),
-      image: image._toEmbedImageBuilder(),
-      thumbnail: thumbnail._toEmbedThumbnailBuilder(),
+      footer: footer?._toEmbedFooterBuilder(),
+      image: image?._toEmbedImageBuilder(),
+      thumbnail: thumbnail?._toEmbedThumbnailBuilder(),
       timestamp: timestamp,
       title: title,
       url: url,
@@ -17,53 +17,22 @@ extension EmbedExtension on Embed {
   }
 }
 
-extension on EmbedAuthor? {
-  EmbedAuthorBuilder? _toEmbedAuthorBuilder() {
-    return switch (this) {
-      EmbedAuthor(:final name, :final iconUrl?, :final url?) => EmbedAuthorBuilder(
-          name: name,
-          iconUrl: iconUrl,
-          url: url,
-        ),
-      _ => null,
-    };
-  }
+extension on EmbedAuthor {
+  EmbedAuthorBuilder _toEmbedAuthorBuilder() => EmbedAuthorBuilder(name: name, iconUrl: iconUrl, url: url);
 }
 
 extension on EmbedField {
   EmbedFieldBuilder _toEmbedFieldBuilder() => EmbedFieldBuilder(name: name, value: value, isInline: inline);
 }
 
-extension on EmbedFooter? {
-  EmbedFooterBuilder? _toEmbedFooterBuilder() {
-    return switch (this) {
-      EmbedFooter(:final text, :final iconUrl?) => EmbedFooterBuilder(
-          text: text,
-          iconUrl: iconUrl,
-        ),
-      _ => null,
-    };
-  }
+extension on EmbedFooter {
+  EmbedFooterBuilder _toEmbedFooterBuilder() => EmbedFooterBuilder(text: text, iconUrl: iconUrl);
 }
 
-extension on EmbedImage? {
-  EmbedImageBuilder? _toEmbedImageBuilder() {
-    return switch (this) {
-      EmbedImage(:final url) => EmbedImageBuilder(
-          url: url,
-        ),
-      _ => null,
-    };
-  }
+extension on EmbedImage {
+  EmbedImageBuilder _toEmbedImageBuilder() => EmbedImageBuilder(url: url);
 }
 
-extension on EmbedThumbnail? {
-  EmbedThumbnailBuilder? _toEmbedThumbnailBuilder() {
-    return switch (this) {
-      EmbedThumbnail(:final url) => EmbedThumbnailBuilder(
-          url: url,
-        ),
-      _ => null,
-    };
-  }
+extension on EmbedThumbnail {
+  EmbedThumbnailBuilder _toEmbedThumbnailBuilder() => EmbedThumbnailBuilder(url: url);
 }
