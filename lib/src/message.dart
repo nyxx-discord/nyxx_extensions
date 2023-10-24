ffa5abffcc688808c49542219c9f91bc82732dda
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2023-10-25 03:07:08 +0600

    feat: add `MessageUtils.sendReply()` method (#22)
    Co-authored-by: Abitofevrything <54505189+abitofevrything@users.noreply.github.com>

--- lib/src/message.dart
+++ lib/src/message.dart
@@ -4,4 +4,24 @@ import 'package:nyxx_extensions/src/channel.dart';
 extension MessageUtils on Message {
   /// A URL clients can visit to navigate to this message.
   Future<Uri> get url async => Uri.https(manager.client.apiOptions.host, '${(await channel.get()).url.path}/$id');
+
+  /// Sends a reply to the message.
+  Future<Message> sendReply(MessageBuilder builder) {
+    final copiedBuilder = MessageBuilder(
+        allowedMentions: builder.allowedMentions,
+        attachments: builder.attachments,
+        components: builder.components,
+        content: builder.content,
+        embeds: builder.embeds,
+        nonce: builder.nonce,
+        replyId: id,
+        requireReplyToExist: builder.requireReplyToExist,
+        stickerIds: builder.stickerIds,
+        suppressEmbeds: builder.suppressEmbeds,
+        suppressNotifications: builder.suppressNotifications,
+        tts: builder.tts,
+    );
+
+    return channel.sendMessage(copiedBuilder);
+  }
 }
