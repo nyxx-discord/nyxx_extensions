325b4a853f5b66816a417d35d54dc1f7ec18333f
Lina L. <contact@lexedia.moe>
2025-05-13 18:22:44 +0200

    sanitize url markers for embeds (#52)
    Co-authored-by: Szymon Uglis <szymon@l7ssha.xyz>

--- test/unit/sanitizer_test.dart
+++ test/unit/sanitizer_test.dart
@@ -6,10 +6,10 @@ import 'package:test/test.dart';
 const _whitespaceCharacter = "‎";
 
 final sampleContent =
-    '<@1234> test <@!2345> test2 <@&3456> test3 <#4567> test4 <a:test_emoji:5678> test5 <:test_emoji:6789> test6 </test command:123456789123456789>';
-final removed = ' test  test2  test3  test4  test5  test6 ';
+    '<@1234> test <@!2345> test2 <@&3456> test3 <#4567> test4 <a:test_emoji:5678> test5 <:test_emoji:6789> test6 </test command:123456789123456789> test7 <https://dart.dev>';
+final removed = ' test  test2  test3  test4  test5  test6  test7 ';
 final sanitized =
-    '<@${_whitespaceCharacter}1234> test <@${_whitespaceCharacter}2345> test2 <@&${_whitespaceCharacter}3456> test3 <#${_whitespaceCharacter}4567> test4 <${_whitespaceCharacter}a\\:test_emoji\\:5678> test5 <$_whitespaceCharacter\\:test_emoji\\:6789> test6 </${_whitespaceCharacter}test command:123456789123456789>';
+    '<@${_whitespaceCharacter}1234> test <@${_whitespaceCharacter}2345> test2 <@&${_whitespaceCharacter}3456> test3 <#${_whitespaceCharacter}4567> test4 <${_whitespaceCharacter}a\\:test_emoji\\:5678> test5 <$_whitespaceCharacter\\:test_emoji\\:6789> test6 </${_whitespaceCharacter}test command:123456789123456789> test7 <<https://dart.dev>>';
 
 class MockNyxxRest with Mock implements NyxxRest {}
 
